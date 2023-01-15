c2e5f4bf8f6adf627531752f628ec612c2b9f5dd
TBSten <programmingcafeteria@gmail.com>
2023-01-16 00:20:08 +0900

    tailwindのテーマ設定を追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,36 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
+    },
+    extend: {
+      colors: {
+        theme: {
+          0: "#0972eb",
+          1: "#ff742e",
+          success: "#01a739",
+          error: "#b72726",
+        },
+        gray: {
+          0: "#f8f8fa",
+          3: "#f2f2f7",
+          1: "#e9e9f0",
+          2: "#222230",
+        },
+      },
+      borderRadius: {
+        none: "0",
+        sm: "0.125rem",
+        md: "0.25rem",
+        lg: "0.5rem",
+        xl: "9999px",
+        DEFAULT: "0.25rem",
+      },
+    },
   },
   plugins: [],
 }
