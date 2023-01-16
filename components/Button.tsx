d23abac77a0f36e1368a081cf6fc0836410b9ed4
TBSten <programmingcafeteria@gmail.com>
2023-01-17 02:22:44 +0900

    Buttonコンポーネントを追加

---
+++ components/Button.tsx
@@ -0,0 +1,42 @@
+import classNames from "classnames";
+import { FC } from "react";
+
+export type ButtonProps = JSX.IntrinsicElements["button"] & {
+    size?: "sm" | "md" | "lg"
+    elevation?: boolean
+}
+const Button: FC<ButtonProps> = ({
+    size = "sm",
+    elevation = false,
+    className,
+    children,
+    ...buttonProps
+}) => {
+    const p = size === "sm" ? "p-2" : size === "md" ? "p-3" : "p-4"
+    const fontSize = size === "sm" ? "text-base" : size ? "text-xl" : "text-3xl"
+    return (
+        <button
+            className={classNames(className,
+                "bg-theme-0",
+                "text-white",
+                p,
+                fontSize,
+                "rounded",
+                "outline-none",
+                {
+                    "hover:-translate-y-1": elevation,
+                    "hover:shadow-2xl": elevation,
+                },
+                "drop-shadow-2xl", "shadow-black",
+                "transition-all",
+                "hover:bg-opacity-75",
+                "active:bg-opacity-90",
+            )}
+            {...buttonProps}
+        >
+            {children}
+        </button>
+    );
+}
+
+export default Button;
